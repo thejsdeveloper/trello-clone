4c21ffb11d1d57fb2274eb63d5f63887ad8adafb
vikas yadav <>
2020-07-26 18:13:13 +0800

    first commit of final struture of react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppContainer } from './styles';
+
+import { Card } from './Card';
+import { Column } from './Column';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learning react"></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typings"></Card>
+      </Column>
+    </AppContainer>
   );
 }
 
