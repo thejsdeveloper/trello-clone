006e99ba420f87100a8a12dbfc50dacc6aab268e
vikas yadav <thejsdeveloper@gmail.com>
2022-03-16 21:18:39 +0800

    adding items on enter

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -10,12 +10,19 @@ export const NewItemForm = ({ onAdd }: NewItemFormType) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
         onChange={(event) => setText(event.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
