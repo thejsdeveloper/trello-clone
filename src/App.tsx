22be99a16dcb63650680b70dc40b9a16ba72e648
vikas yadav <thejsdeveloper@gmail.com>
2022-03-16 21:59:03 +0800

    using app state in column and card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
+import { useAppState } from "./state/AppStateContext";
 import { AppContainer } from "./styles";
 
 export function App() {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((list) => (
+        <Column key={list.id} {...list} />
+      ))}
+
       <AddNewItem text="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
+import { useAppState } from "./state/AppStateContext";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
+  id: string;
   text: string;
-  children?: React.ReactNode;
 };
 
-export function Column({ text }: ColumnProps) {
+export function Column({ text, id }: ColumnProps) {
+  const { getTasksByListId } = useAppState();
+
+  const tasks = getTasksByListId(id);
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold" />
-      <Card text="Learn TypeScript" />
-      <Card text="Begin to use static typing" />
+      {tasks.map((task) => (
+        <Card key={task.id} {...task} />
+      ))}
       <AddNewItem text="+ Add another card" onAdd={console.log} dark />
     </ColumnContainer>
   );
