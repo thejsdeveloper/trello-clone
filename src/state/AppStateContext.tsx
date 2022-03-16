47c3f353accafa1154d11edded2a59b687a3d9e1
vikas yadav <thejsdeveloper@gmail.com>
2022-03-16 21:51:26 +0800

    adding app state context

---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,67 @@
+import React, { createContext, FC, useContext } from "react";
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn Typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing" }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+};
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+export const AppStateProvider: FC = (props) => {
+  const { lists } = appData;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }} {...props} />
+  );
+};
+
+export const useAppState = () => {
+  const context = useContext(AppStateContext);
+
+  if (!context) {
+    throw new Error(`useAppState must be used within AppStateProvider`);
+  }
+
+  return context;
+};
